0cd0f4bc9ea7cf5149c678695782245c0e5d0511
Chamelee <chxxxlxx@gmail.com>
2023-07-05 13:17:47 +0530

    Display Menu Options

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("""
+                                
+                    Foodies Fave Food center
+                
+                    101 or VEQ: View all Empty Queues.
+                    102 or ACQ: Add customer to a Queue.
+                    103 or RCQ: Remove a customer from a Queue. (From a specific location)
+                    104 or PCQ: Remove a served customer.
+                    105 or VCS: View Customers Sorted in alphabetical order (Do not use library sort routine)
+                    106 or SPD: Store Program Data into file.
+                    107 or LPD: Load Program Data from file.
+                    108 or STK: View Remaining burgers Stock.
+                    109 or AFS: Add burgers to Stock.
+                    999 or EXT: Exit the Program.
+                """);
     }
 }
